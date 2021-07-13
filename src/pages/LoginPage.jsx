cdde491f9f0dcc0112ba7248c5cb8953851ea1f6
Diizzii <dieter@kribbel.at>
2021-07-13 15:58:02 +0200

    footer-added

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,23 +4,26 @@ import { Redirect } from 'react-router-dom'
 import { AuthContext } from '../context/AuthContext'
 import Login from '../components/Login'
 import logo from '../images/diaryApp.png'
+import Footer from '../components/Footer'
 
 const LoginPage = () => {
   const { uid } = useContext(AuthContext)
   if (uid) return <Redirect to='/entries' />
 
   return (
-    <div className='custom-form' style={{ marginTop: '12%' }}>
-      <div style={{ textAlign: 'center' }}>
-        <img
-          src={logo}
-          alt='diaryApp'
-          style={{ width: '40px', heigth: '40px' }}
-        />
+    <>
+      <div className='custom-form' style={{ marginTop: '12%' }}>
+        <div style={{ textAlign: 'center' }}>
+          <img
+            src={logo}
+            alt='diaryApp'
+            style={{ width: '40px', heigth: '40px' }}
+          />
+        </div>
+        <Login />
       </div>
-
-      <Login />
-    </div>
+      <Footer />
+    </>
   )
 }
 
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -4,22 +4,26 @@ import { Redirect } from 'react-router-dom'
 import Signup from '../components/Signup'
 import { AuthContext } from '../context/AuthContext'
 import logo from '../images/diaryApp.png'
+import Footer from '../components/Footer'
 
 const SignupPage = () => {
   const { uid } = useContext(AuthContext)
   if (uid) return <Redirect to='/entries' />
 
   return (
-    <div className='custom-form'>
-      <div style={{ textAlign: 'center' }}>
-        <img
-          src={logo}
-          alt='diaryApp'
-          style={{ width: '40px', heigth: '40px' }}
-        />
+    <>
+      <div className='custom-form'>
+        <div style={{ textAlign: 'center' }}>
+          <img
+            src={logo}
+            alt='diaryApp'
+            style={{ width: '40px', heigth: '40px' }}
+          />
+        </div>
+        <Signup />
       </div>
-      <Signup />
-    </div>
+      <Footer />
+    </>
   )
 }
 
